d12bca5ab9c3e52f4c0e90927963c6554299a6db
Josantonius <info@josantonius.com>
2017-01-31 03:26:23 +0100

    Library modified to be compatible with PHP 5.6 or higher

--- src/Exception/FileException.php
+++ src/Exception/FileException.php
@@ -1,4 +1,4 @@
-<?php declare(strict_types=1);
+<?php
 /**
  * PHP library for file management.
  * 
@@ -35,7 +35,7 @@ class FileException extends \Exception {
      * @param int    $error  → error code (Optional)
      * @param int    $status → HTTP response status code (Optional)
      */
-    public function __construct(string $msg = '', int $error = 0, int $status = 0) {
+    public function __construct($msg = '', $error = 0, $status = 0) {
 
         $this->message    = $msg;
         $this->code       = $error;
--- src/File.php
+++ src/File.php
@@ -1,4 +1,4 @@
-<?php declare(strict_types=1);
+<?php
 /**
  * PHP library for file management.
  * 
@@ -8,9 +8,9 @@
  * @author     Josantonius - info@josantonius.com
  * @copyright  Copyright (c) 2017 JST PHP Framework
  * @license    https://opensource.org/licenses/MIT - The MIT License (MIT)
- * @version    1.0.0
+ * @version    1.1.0
  * @link       https://github.com/Josantonius/PHP-File
- * @since      File available since 1.0.0 - Update: 2017-01-17
+ * @since      File available since 1.0.0 - Update: 2017-01-30
  */
 
 namespace Josantonius\File;
@@ -34,7 +34,7 @@ class File {
      *
      * @return bool
      */
-    public static function searchString(string $search, string $pathfile): bool {
+    public static function searchString($search, $pathfile) {
         
         $file = fopen($pathfile, "r+");
  
